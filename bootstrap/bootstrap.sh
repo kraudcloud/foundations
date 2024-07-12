aceb7ca5b5a68d430013cd6499a2cd625fd28923
Arvid E. Picciani <arvid@kraud.cloud>
2024-07-12 21:33:05 +0200

    squishy

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+HERE=$(dirname $(readlink -f $0))
+set -ex
+
+#TODO wish we could bundle this in a single apply, but
+# - 'helm template' is not emitting the labels helm needs to upgrade it later 
+# - helm dependencies are just terrible
+
+kubectl create namespace foundations
+helm install --namespace foundations cilium cilium/cilium --version 1.15.7 --values ${HERE}/cilium-values.yaml
+flux install --namespace foundations
+helm install --namespace foundations foundations  ${HERE}/../charts/foundations --version 0.1.0
