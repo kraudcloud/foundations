86c24b9ce3a3f03bdec83895a63c205a5d2a7f75
Karitham <kar@karitham.dev>
2024-07-16 16:30:12 +0200

    move to kustomize

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 HERE=$(dirname $(readlink -f $0))
-cilium_version=1.15.7
-flux_version=2.13.0
 set -ex
 
 #TODO wish we could bundle this in a single apply, but
@@ -11,16 +9,11 @@ set -ex
 
 kubectl create namespace foundations
 kubectl apply -f ${HERE}/user-skeleton.yaml
-kubectl apply -f ${HERE}/bootstrap-sa.yaml
 
 # helm repo add cilium https://helm.cilium.io/
-helm install --namespace foundations cilium cilium/cilium --version $cilium_version --values ${HERE}/cilium-values.yaml
+helm install --namespace foundations cilium cilium/cilium --version 1.15.7 --values ${HERE}/cilium-values.yaml
 
-# helm repo add fluxcd https://fluxcd-community.github.io/helm-charts
-flux_yaml=$(mktemp)
-kubectl get configmap foundations-config -n foundations -o jsonpath='{.data.flux\.yaml}' >$flux_yaml
-helm install --namespace foundations flux fluxcd/flux2 --version $flux_version --values $flux_yaml --wait --timeout 120s
-rm $flux_yaml
+kubectl apply -k ${HERE}/../flux
 
 foundations_yaml=$(mktemp)
 kubectl get configmap foundations-config -n foundations -o jsonpath='{.data.foundations\.yaml}' >$foundations_yaml
