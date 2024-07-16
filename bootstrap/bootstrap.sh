1bdb3b3a2a76d7d379301e66fea665e0a2bbe3aa
Arvid E. Picciani <arvid@kraud.cloud>
2024-07-16 11:46:44 +0200

    userconfig

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -8,6 +8,8 @@ set -ex
 # - helm dependencies are just terrible
 
 kubectl create namespace foundations
+kubectl apply -f ${HERE}/user-skeleton.yaml
 helm install --namespace foundations cilium cilium/cilium --version 1.15.7 --values ${HERE}/cilium-values.yaml
 flux install --namespace foundations
 helm install --namespace foundations foundations  ${HERE}/../charts/foundations --version 0.1.0
+
