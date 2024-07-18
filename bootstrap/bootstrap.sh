b53dbb72303d0281ddaf89399a27fcbcce00fe72
Karitham <kar@karitham.dev>
2024-07-18 10:09:02 +0200

    move foundations config into foundations kustomize

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -7,12 +7,11 @@ set -ex
 # - 'helm template' is not emitting the labels helm needs to upgrade it later
 # - helm dependencies are just terrible
 
-kubectl create namespace foundations
-kubectl apply -f ${HERE}/user-skeleton.yaml
+kubectl create namespace --save-config foundations
 
 # helm repo add cilium https://helm.cilium.io/
 helm install --namespace foundations cilium cilium/cilium --version 1.15.7 --values ${HERE}/cilium-values.yaml
 
 kubectl apply --namespace foundations --kustomize ${HERE}/../flux
 
-helm install --namespace foundations foundations ${HERE}/../charts/foundations
+kubectl apply --namespace foundations --kustomize ${HERE}/../foundations
